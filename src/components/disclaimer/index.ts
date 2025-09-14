2b697438e59327c34dac2d0895a68011ab839b4d
machariarobert <machariarobert894@gmail.com>
2025-09-15 00:29:48 +0300

    feat: Add @vercel/analytics dependency and import in Disclaimer component

--- src/components/disclaimer/index.ts
+++ src/components/disclaimer/index.ts
@@ -1,2 +1,3 @@
 export { default as DisclaimerButton } from './disclaimer-button';
 export { default as DisclaimerPopup } from './disclaimer-popup';
+import { Analytics } from "@vercel/analytics/next"
