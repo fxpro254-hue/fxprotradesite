987376bbad95bda5372ec494296e3af3f7857dc4
machariarobert <machariarobert894@gmail.com>
2025-11-11 22:06:34 +0300

    fix: add spacing in AppWrapper component for improved layout

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1233,6 +1233,7 @@ const AppWrapper = observer(() => {
                                     </button>
                                 )}
                             </div>
+                            
                         </div>
                         <div
                             label={
